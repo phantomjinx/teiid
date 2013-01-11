582bb35ff2d3836a35347faff0192503e914531b
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2013-01-11 14:37:19 +0000

    TEIIDDES-1565: Remove unnecessary modules and dependencies

--- build/kits/embedded/examples/embedded-portfolio/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TEIID_PATH=../../lib/*:../../optional/translator-file-${pom.version}.jar:../../optional/connector-file-${pom.version}-lib.jar:../../optional/translator-jdbc-${pom.version}.jar:h2-1.3.161.jar
-
-javac -cp ${TEIID_PATH} src/org/teiid/example/*.java 
-
-java -cp ./src:${TEIID_PATH} org.teiid.example.TeiidEmbeddedPortfolio "$@"
