eace1b15304b33237605ee6e2610e6f327422069
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2013-02-05 15:57:57 +0000

    TEIIDDES-1571: Add TeiidXmlFileinfo support to ProcedureService
    * Add the SQL statement to the ProcedureService, generated for XML file
      models.

--- designer/org/teiid82/sql/proc/ProcedureService.java
+++ designer/org/teiid82/sql/proc/ProcedureService.java
@@ -13,6 +13,8 @@ import org.eclipse.osgi.util.NLS;
 import org.teiid.designer.query.IProcedureService;
 import org.teiid.designer.query.proc.ITeiidColumnInfo;
 import org.teiid.designer.query.proc.ITeiidMetadataFileInfo;
+import org.teiid.designer.query.proc.ITeiidXmlColumnInfo;
+import org.teiid.designer.query.proc.ITeiidXmlFileInfo;
 import org.teiid.designer.query.sql.ISQLConstants;
 import org.teiid82.runtime.Messages;
 
@@ -116,6 +118,124 @@ public class ProcedureService implements IProcedureService, ISQLConstants {
         tokens.add(alias);
         
         String finalSQLString = NLS.bind(Messages.procedureServiceTextTableSqlTemplate, tokens.toArray(new String[0]));
+        return finalSQLString;
+    }
+    
+    @Override
+    public String getSQLStatement(ITeiidXmlFileInfo xmlFileInfo, String relationalModelName) {
+        /*
+        ##  SELECT
+        ##      title.pmid AS pmid, title.journal AS journal, title.title AS title
+        ##  FROM
+        ##      (EXEC getMeds.getTextFiles('medsamp2011.xml')) AS f, 
+        ##           XMLTABLE('$d/MedlineCitationSet/MedlineCitation' PASSING 
+        ##                 XMLPARSE(DOCUMENT f.file) AS d 
+        ##                 COLUMNS pmid biginteger PATH 'PMID', journal string PATH 'Article/Journal/Title', title string PATH 'Article/ArticleTitle') AS title
+        ##
+        ## XMLTABLE([<NSP>,] xquery-expression [<PASSING>] [COLUMNS <COLUMN>, ... )] AS name
+        ##
+        ## COLUMN := name (FOR ORDINALITY | (datatype [DEFAULT expression] [PATH string]))
+        ##
+        ##  name     datatype  PATH            string
+        ## journal    string   PATH   'Article/Journal/Title'
+        ##
+        ##
+        ##
+        ##  IF URL XML file, use invokeHTTP() method
+        ##
+        ## EXAMPLE:
+        
+            EXEC SampleSource.getTextFiles('sample.xml')) AS f, 
+                XMLTABLE(XMLNAMESPACES('http://www.kaptest.com/schema/1.0/party' AS pty), 
+                '/pty:students/student' PASSING XMLPARSE(DOCUMENT f.file) 
+                COLUMNS firstName string PATH '/firstName', middleName string PATH '/middleName', lastName string PATH '/lastName') AS A
+        ##
+        ##
+        ## XMLNAMESPACES('http://www.kaptest.com/schema/1.0/party' AS pty)
+        
+        */
+        List<String> tokens = new ArrayList<String>();
+        List<ITeiidXmlColumnInfo> columnInfoList = xmlFileInfo.getColumnInfoList();
+        
+        String alias = "A"; //$NON-NLS-1$
+        StringBuffer sb = new StringBuffer();
+        int i=0;
+        int nColumns = columnInfoList.size();
+        for( ITeiidXmlColumnInfo columnInfo : columnInfoList) {
+            String name = columnInfo.getSymbolName();
+            sb.append(alias).append(DOT).append(name).append(SPACE).append(AS).append(SPACE).append(name);
+            
+            if(i < (nColumns-1)) {
+                sb.append(COMMA).append(SPACE);
+            }
+            i++;
+        }
+        tokens.add(sb.toString());
+        tokens.add(relationalModelName);
+        
+        String proc = S_QUOTE + xmlFileInfo.getDataFile().getName() + S_QUOTE;
+        if( xmlFileInfo.isUrl() ) {
+            proc = S_QUOTE + GET + S_QUOTE
+                    + COMMA + SPACE + NULL
+                    + COMMA + SPACE + S_QUOTE + xmlFileInfo.getXmlFileUrl() + S_QUOTE
+                    + COMMA + SPACE + S_QUOTE + TRUE + S_QUOTE;
+        }
+        tokens.add(proc);
+        
+        String namespaceStr = xmlFileInfo.getNamespaceString();
+        sb = new StringBuffer();
+        
+        if( namespaceStr != null ) {
+            sb.append(namespaceStr);
+        }
+        
+        String xQueryExp = DEFAULT_XQUERY;
+        String rootPath = xmlFileInfo.getRootPath();
+        if( rootPath != null && rootPath.length() > 0 ) {
+            xQueryExp = rootPath;
+        }
+        sb.append(S_QUOTE).append(xQueryExp).append(S_QUOTE);
+        
+        tokens.add(sb.toString());
+        
+        sb = new StringBuffer();
+        i=0;
+        for( ITeiidXmlColumnInfo columnInfo : columnInfoList) {
+            if( columnInfo.getOrdinality() ) {
+                sb.append(columnInfo.getSymbolName()).append(SPACE).append(FOR_ORDINALITY);
+            } else {
+                sb.append(columnInfo.getSymbolName()).append(SPACE).append(columnInfo.getDatatype());
+                
+                String defValue = columnInfo.getDefaultValue();
+                if( defValue != null && defValue.length() > 0) {
+                    sb.append(SPACE).append(DEFAULT).append(SPACE).append(S_QUOTE).append(defValue).append(S_QUOTE);
+                }
+                
+                String relPath = columnInfo.getRelativePath();
+                if( relPath != null && relPath.length() > 1 ) {
+                    sb.append(SPACE).append(PATH).append(SPACE).append(S_QUOTE).append(relPath).append(S_QUOTE);
+                }
+                
+                
+            }
+            if(i < (nColumns-1)) {
+                sb.append(COMMA).append(SPACE);
+            }
+
+            i++;
+        }
+
+        tokens.add(sb.toString());
+        tokens.add(alias);
+
+        String finalSQLString = null;
+        
+        if( xmlFileInfo.isUrl() ) {
+            // SELECT {0} FROM (EXEC {1}.getTextFiles({2})) AS f, XMLTABLE('{3}' PASSING XMLPARSE(DOCUMENT f.file) AS d COLUMNS {4}) AS {5}
+            finalSQLString = NLS.bind(Messages.procedureServiceXmlInvokeHttpTableSqlTemplate, tokens.toArray(new String[0]));
+        } else {
+            finalSQLString = NLS.bind(Messages.procedureServiceXmlGetTextFilesTableSqlTemplate, tokens.toArray(new String[0]));
+        }
         return finalSQLString;
     }
 
