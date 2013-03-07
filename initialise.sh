903bbdd5cbc970a1b853daa60a08b0cf435d2c49
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2013-03-07 12:23:22 +0000

    Fixes lack of calling of the maven source builder
    * Calls the SQLParser source and library builder during auto-builds
      to improve its usability
    
    * initialise.sh
     * Avoids impairing performance with the more frequent executions by
       checking whether all the libraries are present and the SQLParser source
       has been generated

--- initialise.sh
+++ initialise.sh
@@ -17,6 +17,53 @@ GIT_HOME=`cd "../../.."; pwd`
 SCRIPTS_HOME="$GIT_HOME/scripts"
 MVN="$SCRIPTS_HOME/maven-wrapper.sh"
 
+LIBS=(jboss-marshalling.jar \
+			jta.jar \
+			xnio-nio.jar \
+			jboss-vfs.jar \
+			xom.jar \
+			jboss-as-controller-client.jar \
+			jgroups.jar \
+			jboss-threads.jar \
+			jboss-as-protocol.jar \
+			jboss-dmr.jar \
+			jboss-as-cli.jar \
+			jboss-modules.jar \
+			nux.jar \
+			connector-api.jar \
+			jboss-sasl.jar \
+			xnio-api.jar \
+			jboss-logging.jar \
+			jboss-marshalling-river.jar \
+			jboss-remoting.jar \
+			json-simple.jar \
+			rhq-pluginAnnotations.jar)
+
+SQLPARSER="engine/src/main/javacc/org/teiid/query/parser/SQLParser.java"
+
+PROCESS="no"
+
+# Check to see if all the libraries have been downloaded
+for i in ${!LIBS[*]}
+do
+  LIB=${LIBS[$i]}
+
+	if [ ! -f lib/$LIB ]; then
+		PROCESS="yes"
+		break
+	fi
+done
+
+# Check to see that the SQLParser has been generated
+if [ ! -f $SQLPARSER ]; then
+	PROCESS="yes"
+fi
+
+if [ "x$PROCESS" == "xno" ]; then
+	echo "Project source generation is up-to-date"
+	exit 0
+fi
+
 # Build the target platform and install it
 echo "=== Installing target platform ==="
 cd "$GIT_HOME/target-platform"
@@ -27,5 +74,6 @@ echo "=== Installing the spi plugin dependency ==="
 cd "$GIT_HOME/plugins/org.teiid.designer.spi"
 $MVN install
 
+echo "=== Generating external libraries and sources ==="
 cd "$PLUGIN_HOME"
 $MVN process-sources -Pgenerate-javacc
