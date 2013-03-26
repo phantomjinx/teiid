3e40682f63770abbf14c07dd24ed28339d2f0653
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2013-03-26 12:27:05 +0000

    TEIIDDES-1614: Ensure CreateProcedureCommand project symbols is populated
    * Use the post-resolve method to ensure that the projected symbols
      collection of CreateProcedureCommand has been populated with the contents
      of the resultSetColumns collection

--- designer/org/teiid83/sql/impl/validator/WrappedQueryResolver.java
+++ designer/org/teiid83/sql/impl/validator/WrappedQueryResolver.java
@@ -10,9 +10,12 @@ package org.teiid83.sql.impl.validator;
 import java.util.List;
 import org.teiid.designer.query.IQueryResolver;
 import org.teiid.designer.query.metadata.IQueryMetadataInterface;
+import org.teiid.designer.query.sql.lang.ICommand;
 import org.teiid.designer.query.sql.symbol.IElementSymbol;
 import org.teiid.query.resolver.QueryResolver;
 import org.teiid.query.sql.lang.Command;
+import org.teiid.query.sql.proc.CreateProcedureCommand;
+import org.teiid.query.sql.symbol.Expression;
 import org.teiid.query.sql.symbol.GroupSymbol;
 import org.teiid83.sql.impl.CrossQueryMetadata;
 
@@ -32,7 +35,22 @@ public class WrappedQueryResolver implements IQueryResolver<Command, GroupSymbol
     @Override
     public void postResolveCommand(Command command, GroupSymbol gSymbol, int commandType,
                                    IQueryMetadataInterface metadata, List<IElementSymbol> projectedSymbols) {
-        // Nothing required
-    }
 
+        if (! (command instanceof CreateProcedureCommand))
+            return;
+
+        /*
+         * Ensure the contents of CreateProcedureCommand's project symbol collection has been
+         * populated with the contents of its resultSetColumns collection
+         */
+        CreateProcedureCommand procCommand = (CreateProcedureCommand) command;
+        if (ICommand.TYPE_UPDATE_PROCEDURE != procCommand.getType())
+            return;
+
+        List<? extends Expression> resultSetColumns = procCommand.getResultSetColumns();
+        if (resultSetColumns.isEmpty())
+            return;
+
+        procCommand.setProjectedSymbols(resultSetColumns);
+    }
 }
